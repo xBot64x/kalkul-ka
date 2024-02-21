9809dfb7cbda7ec09b57e92093b15a08b11fc096
Filip Kníže <filipknize@gmail.com>
2024-02-21 18:53:03 +0100

    paměť a změna fontu
    změna stylů a paměti

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+session_start(); // začni session
+
+if (isset($_POST['submit'])) { // načti uložená data jestli jsou přítomná
+    $_SESSION['num1'] = $_POST['num1'];
+    $_SESSION['num2'] = $_POST['num2'];
+    $_SESSION['op'] = $_POST['op'];
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,22 +18,22 @@
 </head>
 <body>
 <marquee>nemám rád php</marquee>
-<form method="post" select="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
     <div class="form_row">
-        <input type="number" step=any id="num1" name="num1" required><br>
+        <input type="number" step="any" name="num1" value="<?php echo $_SESSION['num1']; ?>" required><br>
         <select id="op" name="op">
-            <option value="+">plus</option>
-            <option value="-">mínus</option>
-            <option value="*">krát</option>
-            <option value="/">děleno</option>
-            <option value="m">mocnina</option>
-            <option value="oh">odmocnina</option>
+            <option value="+" <?php echo $_SESSION['op'] == '+' ? 'selected' : ''; ?>>plus</option>
+            <option value="-" <?php echo $_SESSION['op'] == '-' ? 'selected' : ''; ?>>mínus</option>
+            <option value="*" <?php echo $_SESSION['op'] == '*' ? 'selected' : ''; ?>>krát</option>
+            <option value="/" <?php echo $_SESSION['op'] == '/' ? 'selected' : ''; ?>>děleno</option>
+            <option value="m" <?php echo $_SESSION['op'] == 'm' ? 'selected' : ''; ?>>mocnina</option>
+            <option value="oh" <?php echo $_SESSION['op'] == 'oh' ? 'selected' : ''; ?>>odmocnina</option>
         </select>
-        <input type="number" step=any id="num2" name="num2" required><br>
+        <input type="number" step="any" name="num2" value="<?php echo $_SESSION['num2']; ?>" required><br>
         <p>=</p>
         <div class="box">
-            <?php include 'kalkulacka.php'; ?>
+            <?php include 'kalkulacka.php'; ?> <!-- výsledek -->
         </div>
     </div>
 
--- kalkulacka.php
+++ kalkulacka.php
@@ -1,41 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalkulátor</title>
+</head>
+<body></body>
+
 <?php
-    if (isset($_POST['submit'])) {   // vstup
+if (isset($_POST['submit'])) { // Check if form is submitted
+    // Retrieve form data from session variables
+    $num1 = $_SESSION['num1'];
+    $num2 = $_SESSION['num2'];
+    $op = $_SESSION['op'];
 
-        // nastavení hodnot
-        $num1 = $_POST['num1'];
-        $num2 = $_POST['num2'];
-        $op = $_POST['op'];
-
-        // výpočty
-        switch ($op) {
-            case "+":
-                $result = $num1 + $num2;
-                break;
-            case "-":
-                $result = $num1 - $num2;
-                break;
-            case "*":
-                $result = $num1 * $num2;
-                break;
-            case "/":
-                if ($num2 == 0) {
-                    die("Nesmíš dělit nulou");
-                } 
-                else {
-                    $result = $num1 / $num2;
-                }
-                break;
-            case "m":
-                $result = pow($num1, $num2);
-                break;
-            case "oh":
-                $result = pow($num2, 1/$num1);
-                break;
-            default:
-                // chyba
-                die("Chybička se vloudila");
-        }
-
-        echo "<p>{$result}</p>";
+    // Perform calculation based on form data
+    switch ($op) {
+        case "+":
+            $result = $num1 + $num2;
+            break;
+        case "-":
+            $result = $num1 - $num2;
+            break;
+        case "*":
+            $result = $num1 * $num2;
+            break;
+        case "/":
+            if ($num2 == 0) {
+                die("Nesmíš dělit nulou");
+            } else {
+                $result = $num1 / $num2;
+            }
+            break;
+        case "m":
+            $result = pow($num1, $num2);
+            break;
+        case "oh":
+            $result = pow($num2, 1 / $num1);
+            break;
+        default:
+            die("Chybička se vloudila"); // Handle error
     }
+
+    echo "<p>{$result}</p>"; // Display result
+}
 ?>
